f19e8d689253e4c143afbc815dc18e90881d274a
Sushil Maurya <104303944+Sushil-171M@users.noreply.github.com>
2024-03-05 22:27:11 +0530

    Practice getElementById

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 # app.js
 
+#------getElementById--------------------------
 
-This will be the entry point For any project or application
+// Write your code below:
+
+ var header = document.getElementById('main-heading')
+ header.textContent = 'Fruit World'
+header.style.color = 'orange'
+var header2 = document.getElementById('header')
+header2.style.backgroundColor= 'green'
+header2.style.borderBottom = '5px solid orange'
+var header3 = document.getElementById('basket-heading')
+header3.style.color= 'green'
+var header4 = document.getElementById('thanks')
+header4.innerHTML= '<p>Please visit us again</p>'
+
+
