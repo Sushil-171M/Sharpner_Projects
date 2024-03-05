a77ddc611bf77210e46500dea412876852404f04
Sushil Maurya <104303944+Sushil-171M@users.noreply.github.com>
2024-03-06 00:06:58 +0530

    practice querySelector and querySelectorAll

--- app.js
+++ app.js
@@ -44,4 +44,43 @@ fruits[4].style.color = 'blue'
 for(let i=0;i<fruits.length;i++)
    fruits[i].style.fontStyle = 'italic'
 
+//---------------querySelector-----------------querySelectorAll---------
+
+// Write the code as shown in the video below:
+// Write answer to the questions asked below:
+
+// 1. Implement the code as shown in the video.
+
+// 2. Use id as query to select the basket heading and set its color to brown.
+
+// 3. Change the background color of even fruit items to red and change their text color to white.
+
+let mainheading = document.querySelector('#main-heading')
+mainheading.style.textAlign = 'right'
+
+let fruits1 = document.querySelector('.fruits')
+  fruits1.style.backgroundColor = 'gray';
+  fruits1.style.padding = '30px';
+fruits1.style.margin = '30px';
+fruits1.style.width = '50%';
+fruits1.style.borderRadius = '10px';
+//-----------------------------------------------------------------------
+let basketheading = document.querySelector('#basket-heading')
+basketheading.style.marginLeft = '30px'
+basketheading.style.color = 'brown'
+
+let fruits2 = document.querySelectorAll('.fruit')
+
+for(let i=0;i<fruits2.length;i++){
+   fruits2[i].style.padding = '10px'
+   fruits2[i].style.margin = '10px'
+   fruits2[i].style.borderRadius = '5px'
+  if(i%2==0)
+    fruits2[i].style.backgroundColor = 'lightgray'
+  else{
+     fruits2[i].style.backgroundColor = 'brown'
+     fruits2[i].style.color = 'white'    
+  }
+ }
+
 
