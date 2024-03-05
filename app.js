5287c2c32f3d72e9ba1fc116c0e8f4d493ce96d7
Sushil Maurya <104303944+Sushil-171M@users.noreply.github.com>
2024-03-05 22:53:23 +0530

    added getElementsByTagName

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var header4 = document.getElementById('thanks')
 header4.innerHTML= '<p>Please visit us again</p>'
 
 
-// ----------getElementsByClassName---------------------
+// ----------getElementsByClassName---(return collection of HTML)-------------------
 
 // Write your code below:
 
@@ -29,5 +29,19 @@ fruit[2].style.backgroundColor = 'yellow'
 
 for(let i= 0;i<fruit.length;i++)
    fruit[i].style.fontWeight = 'bold'
+
+// ---------------getElementsByTagName----(return collection of HTML)---------------
+
+// Write your code below:
+
+// 1. Change the color of 5th "li" tag (one with "Mango" written inside it) to blue.
+
+// 2. Make all the "li" tags italic.
+var fruits = document.getElementsByTagName('li')
+
+fruits[4].style.color = 'blue'
+
+for(let i=0;i<fruits.length;i++)
+   fruits[i].style.fontStyle = 'italic'
 
 
